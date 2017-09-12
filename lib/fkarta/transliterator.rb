2c2469be2e55afba5a17ae17706f667ca029d028
Stas Iskandarov <stas@veeqo.com>
2017-09-12 23:45:07 +0600

    Refactored transliterator

--- lib/fkarta/transliterator.rb
+++ lib/fkarta/transliterator.rb
@@ -1,62 +1,59 @@
 class Transliterator
 
   def self.transliterate(string)
-    transliterated_string = ''
-
-    string.each_char { |char| transliterated_string += transliterate_char(char) }
-
-    transliterated_string
+    string.split { |char| transliterate_char(char) }.join('')
   end
 
   private
 
+  DICTIONARY = {
+      'А' => 'A',
+      'Б' => 'B',
+      'В' => 'V',
+      'Г' => 'G',
+      'Д' => 'D',
+      'Е' => 'E',
+      'Ё' => 'E',
+      'Ж' => 'ZH',
+      'З' => 'Z',
+      'И' => 'I',
+      'Й' => 'Y',
+      'К' => 'K',
+      'Л' => 'L',
+      'М' => 'M',
+      'Н' => 'N',
+      'О' => 'O',
+      'П' => 'P',
+      'Р' => 'R',
+      'С' => 'S',
+      'Т' => 'T',
+      'У' => 'U',
+      'Ф' => 'F',
+      'Х' => 'KH',
+      'Ц' => 'TS',
+      'Ч' => 'CH',
+      'Ш' => 'SH',
+      'Щ' => 'SHCH',
+      'Ъ' => '',
+      'Ы' => 'Y',
+      'Ь' => '',
+      'Э' => 'E',
+      'Ю' => 'YU',
+      'Я' => 'YA',
+      '0' => '0',
+      '1' => '1',
+      '2' => '2',
+      '3' => '3',
+      '4' => '4',
+      '5' => '5',
+      '6' => '6',
+      '7' => '7',
+      '8' => '8',
+      '9' => '9',
+  }
+
   def self.transliterate_char(char)
-    case char
-      when 'А' then return 'A'
-      when 'Б' then return 'B'
-      when 'В' then return 'V'
-      when 'Г' then return 'G'
-      when 'Д' then return 'D'
-      when 'Е' then return 'E'
-      when 'Ё' then return 'E'
-      when 'Ж' then return 'ZH'
-      when 'З' then return 'Z'
-      when 'И' then return 'I'
-      when 'Й' then return 'Y'
-      when 'К' then return 'K'
-      when 'Л' then return 'L'
-      when 'М' then return 'M'
-      when 'Н' then return 'N'
-      when 'О' then return 'O'
-      when 'П' then return 'P'
-      when 'Р' then return 'R'
-      when 'С' then return 'S'
-      when 'Т' then return 'T'
-      when 'У' then return 'U'
-      when 'Ф' then return 'F'
-      when 'Х' then return 'KH'
-      when 'Ц' then return 'TS'
-      when 'Ч' then return 'CH'
-      when 'Ш' then return 'SH'
-      when 'Щ' then return 'SHCH'
-      when 'Ъ' then return ''
-      when 'Ы' then return 'Y'
-      when 'Ь' then return ''
-      when 'Э' then return 'E'
-      when 'Ю' then return 'YU'
-      when 'Я' then return 'YA'
-      when '0' then return '0'
-      when '1' then return '1'
-      when '2' then return '2'
-      when '3' then return '3'
-      when '4' then return '4'
-      when '5' then return '5'
-      when '6' then return '6'
-      when '7' then return '7'
-      when '8' then return '8'
-      when '9' then return '9'
-      else
-        return 'wrong chars'
-    end
+    DICTIONARY[char.to_sym]
   end
+
 end
